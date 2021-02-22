77a04f81226b3d1e7e073668594d8d2e90bf1d61
lijian <lijian@topscomm.com>
2021-02-22 16:02:48 +0800

    first commit

---
+++ 井字棋/MainForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace 井字棋
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 240);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "井字棋";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 井字棋/MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace 井字棋
+{
+    public partial class MainForm : Form
+    {
+        Button[] Cell = new Button[9];
+        bool Flag = true;
+        int Count = 0;
+        int[] Table = { 0, 4, 8, 2, 4, 6, 0, 1, 2, 3, 4, 5,
+                        6, 7, 8, 0, 3, 6, 1, 4, 7, 2, 5, 8 };
+        int[] Value = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public MainForm()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 9; i++)
+            {
+                Cell[i] = new Button();
+                Cell[i].Tag = i;
+                Cell[i].Location = new Point(80 * (i % 3), 80 * (i / 3));
+                Cell[i].Size = new Size(80, 80);
+                Cell[i].Font = new Font("Arial", 36, Cell[i].Font.Style);
+                Cell[i].Click += new EventHandler(Cell_Click);
+                Cell[i].MouseLeave += new EventHandler(MainForm_Shown);
+                Controls.Add(Cell[i]);
+            }
+        }
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            label1.Focus();
+        }
+        private void Cell_Click(object sender, EventArgs e)
+        {
+            int i = (int)((Button)sender).Tag;
+            if (Value[i]==0)
+            {
+                Cell[i].Text = Flag ? "O" : "X";
+                Value[i] = Flag ? 1 : -1;
+                Flag = !Flag;
+                Check();
+            }
+        }
+        void Check()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                int Base = Value[Table[i * 3]];
+                Base += Value[Table[i * 3 + 1]];
+                Base += Value[Table[i * 3 + 2]];
+                if (Base == 3 || Base == -3)
+                {
+                    MessageBox.Show(Base == 3 ? "Win" : "Lost");
+                    Application.Restart();
+                }
+            }
+            if (Count++ == 8)
+            {
+                MessageBox.Show("Tie");
+                Application.Restart();
+            }
+        }
+    }
+}
